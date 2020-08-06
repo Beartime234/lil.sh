f13c2f13a4aba7e3db021729723776efc93d5a65
beartime234 <josheatonis234@gmail.com>
2020-08-06 21:01:09 +1200

    Adding serverless stuff

---
+++ example.py
@@ -0,0 +1,24 @@
+import os
+
+import boto3
+
+S3_BUCKET = "lil.sh"
+PREFIX = "surl"
+
+
+def main():
+    upload_blank_object()
+
+
+def upload_blank_object():
+    s3 = boto3.client("s3")
+    s3.put_object(
+        Body=b'',
+        Bucket=S3_BUCKET,
+        Key=f"surl/test",
+        WebsiteRedirectLocation="https://google.com"
+    )
+
+
+if __name__ == '__main__':
+    main()
